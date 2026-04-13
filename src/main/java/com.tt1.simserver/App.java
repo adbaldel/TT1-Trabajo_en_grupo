5658e6bca89c977bcdba45122a184859368aa479
jumedrm <medranojuanlu@gmail.com>
2026-04-13 17:02:04 +0200

    HE AÑADIDO COSAS DE REST, CUIDADO

--- src/main/java/com.tt1.simserver/App.java
+++
@@ -1,5 +0,0 @@
-package com.tt1.simserver;
-
-public class App
-{
-}
--- src/main/java/com.tt1.simserver/RestResourceRoot.java
+++
@@ -1,5 +0,0 @@
-package com.tt1.simserver;
-
-public class RestResourceRoot {
-    public static final String APPLICATION_PATH = "";
-}
